716dce285ccfe53dde2a6d4135b1904f0176db60
brsaca <brsaca@gmail.com>
2023-09-28 12:02:06 -0600

    Initial Commit

---
+++ DonationUI/DonationUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DonationUIApp.swift
+//  DonationUI
+//
+//  Created by Brenda Saavedra Cantu on 28/09/23.
+//
+
+import SwiftUI
+
+@main
+struct DonationUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
