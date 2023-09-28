572716a1f05fecc906110bc4976982e587f825d0
brsaca <brsaca@gmail.com>
2023-09-28 12:51:39 -0600

    style: add colors and gradient+

---
+++ DonationUI/Extensions/Color.swift
@@ -0,0 +1,21 @@
+//
+//  Color.swift
+//  DonationUI
+//
+//  Created by Brenda Saavedra Cantu on 28/09/23.
+//
+
+import SwiftUI
+
+extension Color {
+    static let profileBkg = LinearGradient(colors: [Color("topGreen"), Color("lightGreen")], startPoint: .top, endPoint: .bottom)
+    static let lightButton = Color("lightGreen")
+    static let darkButton = Color("darkGreen")
+    static let purpleBorder = Color("purple")
+    static let darkGray = Color("darkGray")
+    static let ligthGray = Color("ligthGray")
+    static let gradientColor = LinearGradient(
+        colors: [.darkButton, .purpleBorder],
+        startPoint: .topLeading,
+        endPoint: .bottomTrailing)
+}
